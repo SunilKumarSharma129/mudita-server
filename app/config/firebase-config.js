607e5ecb3d63b7439ee2b24d6294608eef521969
mitrukahitesh <mitrukahitesh@gmail.com>
2023-03-25 02:24:15 +0530

    update firebase config

--- app/config/firebase-config.js
+++ app/config/firebase-config.js
@@ -1,9 +1,10 @@
 var admin = require("firebase-admin");
+require("dotenv").config();
 
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
