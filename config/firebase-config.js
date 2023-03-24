12d2d14563cbc1e2f70e11080213e55ac972991d
hackeramitkumar <amit9116260192@gmail.com>
2023-03-24 22:33:47 +0530

    updated firebase-admin file

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./service-account.json");
+const serviceAccount = require("process.env.FIREBASE_JSON");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
